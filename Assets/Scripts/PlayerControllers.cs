a099089a25e97d0e9906ecb32244842491fc4edf
Zerty123 <a.fm2@orange.fr>
2023-02-13 13:21:06 +0100

    {Add}Debug Rotation personnage

--- Assets/Scripts/PlayerControllers.cs
+++ Assets/Scripts/PlayerControllers.cs
@@ -8,16 +8,45 @@ public class PlayerControllers : MonoBehaviour
 
     private PlayerControls playerControls;
     private Rigidbody2D myRigidbody;
+    public float speed =2;
+    public float acc = 1;
 
     private void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        
+    }
+
+    public void Update()
+    {
+        //playerControls.Gameplay.Movement_2.performed += ctx => Move_2(ctx);
+        //Debug.Log(transform.position);
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
+        speed = 2;
         Vector2 inputVector = ctx.ReadValue<Vector2>();
-        myRigidbody.velocity = new Vector2(inputVector.x, inputVector.y);
+        //Vector2 speedVector = ctx.ReadValue<Vector2>();
+        //speed = Mathf.Abs(speedVector.x)+Mathf.Abs(speedVector.y);
+        //acc = Mathf.Abs(inputVector.x)+Mathf.Abs(inputVector.y);
+        //speed = speed + acc*Time.deltaTime*8;
+        myRigidbody.velocity = new Vector2(speed*inputVector.x, speed*inputVector.y);
+        //Debug.Log(inputVector.x);
+        if(inputVector.x==1)
+        {
+            Debug.Log("Droite");
+        }
+        if(inputVector.x==-1)
+        {
+            Debug.Log("Gauche");
+        }
+        if(inputVector.y==1)
+        {
+            Debug.Log("Haut");
+        }
+        if(inputVector.y==-1)
+        {
+            Debug.Log("Bas");
+        }
     }
 }
