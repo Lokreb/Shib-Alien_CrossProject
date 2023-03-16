b21d27aef5baa416fba820c575b481c484f75653
heris-fsya <fsya45@gmail.com>
2023-03-16 16:45:22 +0100

    {add} Roll salle de boss et changement d'etage

--- Assets/Scripts/Systems/Salle/RandomRoom.cs
+++ Assets/Scripts/Systems/Salle/RandomRoom.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class RandomRoom : MonoBehaviour
 {
     [System.Serializable]
-    public class ListWarper
+    public class ListWarperRoom
      
     {
         public List<GameObject> RoomList;
     }
-    [SerializeField] List<ListWarper> Floor_List;
-   
+    [SerializeField] List<ListWarperRoom> Floor_List;
+    public class ListWarperBoss
+
+    {
+        public List<GameObject> BossRoomList;
+    }
+    [SerializeField] List<ListWarperBoss> Boss_List;
+
     int randomIndex;
     public int Floor_number = 5;
     int actual_floor = 0;
     int range;
     GameObject resetPrefab = null;
+     int iteration = 0;
+    static public bool GoBoss =false;
 
 
 
@@ -41,10 +49,35 @@ public class RandomRoom : MonoBehaviour
             randomPrefab.SetActive(true);
             resetPrefab = randomPrefab;
             Floor_List[actual_floor].RoomList.RemoveAt(randomIndex);
+        iteration++;
+        if (iteration >= 3)
+        {
+            GoBoss = true;
+        }
+        Debug.Log("AstarPath il passe");
         AstarPath.active.Scan();
 
 
 
+
+    }
+
+    public void SelectBoss()
+        {
+        if (resetPrefab != null)
+        {
+            resetPrefab.SetActive(false);
+            range = Boss_List[actual_floor].BossRoomList.Count;
+            randomIndex = Random.Range(0, range);
+        }
+        GameObject randomPrefab = Boss_List[actual_floor].BossRoomList[randomIndex];
+        randomPrefab.SetActive(true);
+        resetPrefab = randomPrefab;
+        Floor_List[actual_floor].RoomList.RemoveAt(randomIndex);
+        iteration = 0;
+        actual_floor++;
+        GoBoss = false;
+        
 
     }
 
